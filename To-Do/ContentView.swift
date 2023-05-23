2bf20c83b81aad9a2357e0d7a18eeee53fadab93
aaryan1203 <aaryanjain1203@gmail.com>
2023-05-22 18:57:42 -0700

    Created basic page

--- To-Do/ContentView.swift
+++ To-Do/ContentView.swift
@@ -7,13 +7,41 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var toDoItems: [String] = [""]
+    @State var text: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Testing!")
+            HStack {
+                Text("To-Do List")
+                    .font(.title)
+                Image("check")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit )
+                    .frame(width: 35.0, height: 35)
+            }
+            ForEach (toDoItems.indices, id: \.self) {index in
+                HStack {
+                    Button(" ") {
+                        
+                    }
+                    .padding(.horizontal)
+                    TextField("Next Item", text: $text )
+                    Spacer()
+                }
+            }
+
+            Spacer()
+            HStack {
+                Spacer(minLength: 0)
+                Button("New list") {
+                    toDoItems.append("")
+                }
+                Image("check")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
         }
         .padding()
     }
