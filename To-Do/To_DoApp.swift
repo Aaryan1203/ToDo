666263b192c1321037a8ad35f2cb013691619a82
aaryan1203 <aaryanjain1203@gmail.com>
2023-05-22 17:16:49 -0700

    Initial Commit

---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by Aaryan Jain on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
