9217e14e35553b9d6fd134a9d7a36393e79390df
aaryan1203 <aaryanjain1203@gmail.com>
2023-05-22 17:46:35 -0700

    Changed back to content view

--- To-Do/ContentView.swift
+++ To-Do/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct MainView: View {
+struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -21,6 +21,6 @@ struct MainView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        ContentView()
     }
 }
